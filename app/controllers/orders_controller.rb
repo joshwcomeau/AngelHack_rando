6d0f0f8c847b7a603ea083f746a84bf62e2fa110
Josh Comeau <joshwcomeau@gmail.com>
2014-06-21 14:57:40 -0400

    Refactored the API call a little bit.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,30 +15,35 @@ class OrdersController < ApplicationController
 
     cuisines = ['American', 'Sushi']
 
-    find_restaurants(address, cuisines)
+    budget_low = 15
+
+    find_restaurants(address, cuisines, budget_low)
   end
 
 
-  def find_restaurants(address, cuisines)
-    @restaurants = @api.delivery_list(address)
-    render :json => get_valid_restaurants(@restaurants, cuisines)
+  def find_restaurants(address, cuisines, budget_low)
+    # Get a list of all restaurants that deliver to this address from the API
+    @all_restaurants = @api.delivery_list(address)
+
+    # Filter down to the valid restaurants for this purpose
+    @valid_restaurants = get_valid_restaurants(@all_restaurants, cuisines, budget_low)
+
+
+    render :json => @valid_restaurants
   end
 
 
   private
   
-  def get_valid_restaurants(rest, cuisines)
+  def get_valid_restaurants(rest, cuisines, budget_low)
     rest.select do |r|
-      validate_restaurant(r, cuisines)
+      ( 
+        ( rest["is_delivering"] == 1 ) &&
+        ( rest["services"]["deliver"]["time"] < 60 ) && 
+        ( rest["services"]["deliver"]["mino"] >= budget_low ) && 
+        ( cuisines.any? { |cuisine| rest["cu"].include? cuisine } if rest["cu"] ) 
+      )
     end
-  end
-
-  def validate_restaurant(rest, cuisines)
-    ( 
-      ( cuisines.any? { |cuisine| rest["cu"].include? cuisine } if rest["cu"] ) && 
-      ( rest["services"]["deliver"]["time"] < 60 ) && 
-      ( rest["is_delivering"] == 1 )
-    )
   end
 
   def setup_api
